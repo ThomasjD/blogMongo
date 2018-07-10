fb9fc187ca26329f220699f622cc6d6e36ff5f18
ThomasJd <thomasdinh25@gmail.com>
2018-07-10 00:01:29 -0500

    new models

--- Schema/user.js
+++ Schema/user.js
@@ -2,10 +2,12 @@ const moongoose = require('mongoose')
 
 //structure for comments
 let user = mongoose.Schema({
-  username: String,
+  name: String,
+  username: String
 })
 
 //convert schema into a model
-const user = moongoose.model('User', user)
+const User = moongoose.model('User', userSchema)
 
+//make it available in Node application
 module.exports = User
